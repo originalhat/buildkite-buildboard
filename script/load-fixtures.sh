57dfdbe770efcbece855ab0763e9670f825f6e0a
Devin Brown <devin.g.brown@gmail.com>
2017-07-16 14:42:35 -0600

    styling grid

---
+++ script/load-fixtures.sh
@@ -0,0 +1,5 @@
+# primary-finished
+curl -XPOST -H 'Content-Type:application/json' -H 'Accept: application/json' --data-binary @/Users/pipeline/workspace/buildkite-buildboard/src/fixtures/primary-finished.json http://localhost/buildkite
+
+# weekday-running
+curl -XPOST -H 'Content-Type:application/json' -H 'Accept: application/json' --data-binary @/Users/pipeline/workspace/buildkite-buildboard/src/fixtures/weekday-running.json http://localhost/buildkite
--- src/js/index.js
+++ src/js/index.js
@@ -1,19 +1,30 @@
 function renderBuilds(data) {
-  let buildBlock;
+  const filteredData = filteredBuilds(data);
 
-  const buildTemplate = document.querySelector('.build-template');
-  const buildID = `build--${data.build.id}`;
-
-  if (buildElement(buildID)) {
-    buildBlock = buildElement(buildID);
-  } else {
-    buildBlock = buildTemplate.cloneNode(true);
+  if (filteredData) {
+    updateView(buildBlock(data.build.id), filteredData);
   }
+}
 
+function buildBlock(buildID) {
+  const buildClassName = `build--${buildID}`;
+
+  if (buildElement(buildClassName)) {
+    return buildElement(buildClassName);
+  } else {
+    return document.querySelector('.build-template').cloneNode(true);
+  }
+}
+
+function filteredBuilds(data) {
+  if (data.build.branch === 'master') {
+    return data;
+  }
+}
+
+function updateView(buildBlock, data) {
   buildBlock.className = `build build--${data.build.id} build--${data.build.state}`;
-
   rootElement().appendChild(buildBlock);
-
   buildBlock.querySelector('.build__name').innerHTML = data.pipeline.name;
   buildBlock.querySelector('.build__message').innerHTML = data.build.message;
 }
