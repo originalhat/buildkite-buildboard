dede902f34b942b5a2f1b17813a9120673053aa8
Devin Brown <devin.g.brown@gmail.com>
2017-07-16 10:49:56 -0600

    PWS specific tuning

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const server = require('http').Server(app);
 const io = require('socket.io')(server);
 const bodyParser = require("body-parser");
 
-const PORT = 4000; // TODO: accept as ENV
+const PORT = process.env.PORT || 80;
 
 // middleware
 app.use(bodyParser.urlencoded({ extended: false }));
